43d8a1a7505cfc2905b52b7285b6b13d7b031496
Noelia Carrizo <noeliacarrizo22@gmail.com>
2017-05-08 20:03:01 -0300

    Actualizando archivo crear tabla

--- bd/create_table.sql
+++ bd/create_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `caja_diaria` (
-  `caja_diaria_id` int(11) NOT NULL,
+CREATE TABLE `caja_diaria2` (
+  `caja_diaria_id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `fecha` datetime NOT NULL,
   `saldo_inicial` float(8,2) DEFAULT '0.00',
   `movimientos` float(8,2) DEFAULT '0.00',
-  `saldo_final` float(8,2) DEFAULT '0.00'
+  `saldo_final` float(8,2) DEFAULT '0.00',
+    PRIMARY KEY (`caja_diaria_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
