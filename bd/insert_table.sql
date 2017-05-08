74862f16da045fb13a66541f957a5b8d21e3437b
José Fantasia <teseo87@gmail.com>
2017-05-08 10:00:03 -0300

    Ejemplos ampliados

--- bd/insert_table.sql
+++ bd/insert_table.sql
@@ -1,1 +1,5 @@
-INSERT INTO `caja_diaria`(`caja_diaria_id`, `libro_de_caja_id`, `nombre`, `fecha`, `saldo_inicial`, `movimientos`, `saldo_final`) VALUES (1,1,'Movimiento de compra',now(),1,9,10)
+INSERT INTO `caja_diaria`(`caja_diaria_id`, `libro_de_caja_id`, `nombre`, `fecha`, `saldo_inicial`, `movimientos`, `saldo_final`) VALUES (1,1,'Movimiento de compra',now(),1,9,10);
+
+INSERT INTO `caja_diaria`(`caja_diaria_id`, `libro_de_caja_id`, `nombre`, `fecha`, `saldo_inicial`, `movimientos`, `saldo_final`) VALUES (2,1,'Movimiento de compra a fecha con hora','2017-01-20 10:20:30',1,9,10);
+
+INSERT INTO `caja_diaria`(`caja_diaria_id`, `libro_de_caja_id`, `nombre`, `fecha`, `saldo_inicial`, `movimientos`, `saldo_final`) VALUES (3,1,'Movimiento de compra a fecha','2017-01-20',1,9,10);
--- bd/select_table.sql
+++ bd/select_table.sql
@@ -1,1 +1,5 @@
-SELECT * FROM `caja_diaria` WHERE 1
+SELECT * FROM `caja_diaria` WHERE 1;
+
+SELECT `caja_diaria_id`, `libro_de_caja_id`, `nombre`, `fecha`, `saldo_inicial`, `movimientos`, `saldo_final` FROM `caja_diaria` WHERE fecha <= '2014-03-29 00:00:00';
+
+SELECT `nombre`, `saldo_inicial`, `movimientos`, `saldo_final` FROM `caja_diaria` WHERE fecha < '2014-03-29 00:00:00';
